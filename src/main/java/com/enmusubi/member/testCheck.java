66541df792a7606cbd583de049a8b5be28fb190d
kagi <key9910@gmail.com>
2024-06-04 15:39:20 +0900

    Fix: login Check

---
+++ src/main/java/com/enmusubi/member/testCheck.java
@@ -0,0 +1,14 @@
+package com.enmusubi.member;
+
+public class testCheck {
+	private String res;
+
+	public String getRes() {
+		return res;
+	}
+
+	public void setRes(String res) {
+		this.res = res;
+	}
+	
+}
