1c442c2288f25f5350c0fd0b52bd3e7335818e43
kagi <key9910@gmail.com>
2024-06-05 13:13:06 +0900

    Feat:Add nullcheck

---
+++ src/main/java/com/enmusubi/finance/ResultC.java
@@ -0,0 +1,21 @@
+package com.enmusubi.finance;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ResultC")
+public class ResultC extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setAttribute("page", "financeresult.jsp");
+		request.getRequestDispatcher("finance/index.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
