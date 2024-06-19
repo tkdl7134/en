bdcd10fc951a91c89949f36a327be7a3c07a46ac
jungeun <pje961011@gmail.com>
2024-06-19 16:36:35 +0900

    [Feat] 초대장 insert구문 작성중

--- src/main/java/com/enmusubi/product/ProductDAO.java
+++ src/main/java/com/enmusubi/product/ProductDAO.java
@@ -3,9 +3,13 @@ package com.enmusubi.product;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
+
+import org.apache.naming.java.javaURLContextFactory;
 
 import com.enmusubi.main.DBManager;
 import com.oreilly.servlet.MultipartRequest;
@@ -53,20 +57,23 @@ public class ProductDAO {
 		Connection con = null;
 //		PreparedStatement pstmtEventNo = null;
 		PreparedStatement pstmtWeddingInfo = null;
+		PreparedStatement pstmtWedding = null;
 		PreparedStatement pstmtReception = null;
-		PreparedStatement pstmtReception2 = null;
 		ResultSet rs = null;
 		DBManager dbManager = DBManager.getInstance();
 		
 //		create sequence e_no_seq;
-		
+																//	e_no
 		String sqlWeddingInfo = "insert into s_wedding_info values(e_no_seq.nextval, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-
+		String sqlWedding = "insert into s_reception values(s_reception_seq.nextval, e_no_seq.currval, ?, ?, ?, ?, ?)";
+		String sqlReception = "insert into s_reception values(s_reception_seq.nextval, e_no_seq.currval, ?, ?, ?, ?, ?)";
 		
 		try {
 			 con = dbManager.connect();
 			 
 			 pstmtWeddingInfo = con.prepareStatement(sqlWeddingInfo);
+			 pstmtWedding = con.prepareStatement(sqlWedding);
+			 pstmtReception = con.prepareStatement(sqlReception);
 			 
 			 
 			String path = request.getServletContext().getRealPath("product/imgFile");
@@ -86,47 +93,55 @@ public class ProductDAO {
 			hello = hello.replaceAll("\r\n", "<br>");
 			bye = bye.replaceAll("\r\n", "<br>");
 			
-			pstmtWeddingInfo.setString(2, mr.getParameter("templatePK"));
-			pstmtWeddingInfo.setString(3,  groomName_kanji);
-			pstmtWeddingInfo.setString(4,  groomName_kana);
-			pstmtWeddingInfo.setString(5, groomName_roma);
-			pstmtWeddingInfo.setString(6, brideName_kanji);
-			pstmtWeddingInfo.setString(7, brideName_kana);
-			pstmtWeddingInfo.setString(8, brideName_roma);
-			pstmtWeddingInfo.setString(9, hello);
-			pstmtWeddingInfo.setString(10, bye);
-			pstmtWeddingInfo.setString(11, mr.getFilesystemName("photo1"));
-			pstmtWeddingInfo.setString(12, mr.getFilesystemName("photo2"));
-			pstmtWeddingInfo.setString(13, mr.getFilesystemName("photo3"));
-			pstmtWeddingInfo.setString(14, mr.getFilesystemName("photo4"));
+			pstmtWeddingInfo.setString(1, mr.getParameter("templatePK"));
+			pstmtWeddingInfo.setString(2,  groomName_kanji);
+			pstmtWeddingInfo.setString(3,  groomName_kana);
+			pstmtWeddingInfo.setString(4, groomName_roma);
+			pstmtWeddingInfo.setString(5, brideName_kanji);
+			pstmtWeddingInfo.setString(6, brideName_kana);
+			pstmtWeddingInfo.setString(7, brideName_roma);
+			pstmtWeddingInfo.setString(8, hello);
+			pstmtWeddingInfo.setString(9, bye);
+			pstmtWeddingInfo.setString(10, mr.getFilesystemName("photo1"));
+			pstmtWeddingInfo.setString(11, mr.getFilesystemName("photo2"));
+			pstmtWeddingInfo.setString(12, mr.getFilesystemName("photo3"));
+			pstmtWeddingInfo.setString(13, mr.getFilesystemName("photo4"));
 			
 			
+			
+			//s_reception 테이블 삽입 
 			String weddingDay = mr.getParameter("weddingDay");
-			
 			String m_time = mr.getParameter("marriageTime");
-			String m_timeA = mr.getParameter("marriageTime_a");
-			String m_place = mr.getParameter("marriage_place");
-			String m_addr = mr.getParameter("marriage_addr");
-			
 			String r_time = mr.getParameter("receptionTime");
-			String r_timeA = mr.getParameter("receptionTime_a");
-			String r_place = mr.getParameter("reception_place");
-			String r_addr = mr.getParameter("reception_addr");
 			
-			pstmt.setString(2, t_pk);
-			pstmt.setString(3, m_time);
-			pstmt.setString(4, m_timeA);
-			pstmt.setString(5, m_place);
-			pstmt.setString(6, m_addr);
+			String wedding = weddingDay + m_time;
+			String reception = weddingDay + r_time;
 			
+			SimpleDateFormat dateTimeFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+			java.sql.Timestamp weddingDateTime = new java.sql.Timestamp(dateTimeFormat.parse(wedding).getTime());
+			java.sql.Timestamp receptionDateTime = new java.sql.Timestamp(dateTimeFormat.parse(reception).getTime());
 			
+			// 본식
+			pstmtWedding.setTimestamp(1, weddingDateTime);   // weddingday+marriagetime
+			pstmtWedding.setString(2, mr.getParameter("marriage_place"));
+			pstmtWedding.setString(3, mr.getParameter("marriage_addr"));
+			pstmtWedding.setString(4, mr.getParameter("marriageTime_a"));
+			pstmtWedding.setString(5, "wedding");
+			
+			// 본식
+			pstmtWedding.setTimestamp(1, receptionDateTime);   // weddingday+marriagetime
+			pstmtWedding.setString(2, mr.getParameter("reception_place"));
+			pstmtWedding.setString(3, mr.getParameter("reception_addr"));
+			pstmtWedding.setString(4, mr.getParameter("receptionTime_a"));
+			pstmtWedding.setString(5, "reception");
+						
 			
 			
 		} catch (Exception e) {
 		    e.printStackTrace();
 		    System.out.println("server err...");
 		} finally {
-		    dbManager.close(con, pstmt, rs);
+		    dbManager.close(con, pstmtWeddingInfo, rs);
 		}
 		
 	}
