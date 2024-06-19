9fd853c95ba476329fffe778aa0f83a7b7e18672
yujeongstellakim <yujeongstellakim@gmail.com>
2024-06-20 02:07:44 +0900

    [FEAT] modify section 2&3 / add section 6 / set to work on section 7

--- src/main/webapp/main/main.js
+++ src/main/webapp/main/main.js
@@ -58,7 +58,31 @@ s2menuBtn.addEventListener("click", function () {
   }
 });
 
-// Section 2
+// mouse cursor (drag)
+const s2CardConAll = document.querySelectorAll(".yj-main-s2-section");
+const s2Mouseicn = document.querySelector(".yj-main-s2-cursor");
+
+s2CardConAll.forEach((card) => {
+  card.addEventListener("mouseenter", function (event) {
+    s2Mouseicn.querySelector("img").src = "imgFolder/yj-main-s2-drag.png";
+    card.style.cursor = "none";
+    s2Mouseicn.classList.remove("yj-main-s2-cursor-none");
+    s2Mouseicn.classList.add("yj-main-s2-cursor-block");
+  });
+
+  card.addEventListener("mouseleave", function () {
+    s2Mouseicn.classList.remove("yj-main-s2-cursor-block");
+    s2Mouseicn.classList.add("yj-main-s2-cursor-none");
+  });
+});
+
+document.addEventListener("mousemove", (event) => {
+  const mouseX = event.clientX;
+  const mouseY = event.clientY;
+  s2Mouseicn.style.left = mouseX + "px";
+  s2Mouseicn.style.top = mouseY + "px";
+});
+
 // 배경 이미지  슬라이드
 function slickStart() {
   $(".yj-main-s2-bg").css(
@@ -94,7 +118,6 @@ function slickStart() {
   );
 }
 
-// Section 2
 // Single Item Slide
 $(document).ready(function () {
   $(".yj-main-s2-list")
@@ -154,6 +177,91 @@ s3menuBtn.addEventListener("click", function () {
   }
 });
 
+// 체크체크테스트
+// document.addEventListener("DOMContentLoaded", () => {
+//   const textElement = document.getElementById("verticalText");
+//   const text = textElement.textContent;
+//   textElement.textContent = "";
+
+//   for (let i = 0; i < text.length; i++) {
+//     const span = document.createElement("span");
+//     span.textContent = text[i];
+//     span.style.display = "inline-block";
+//     span.style.transform = "translateY(-100%)";
+//     span.style.opacity = "0";
+//     span.style.transition = `transform 0.5s ease, opacity 0.5s ease`;
+//     span.style.transitionDelay = `${i * 0.1}s`;
+//     textElement.appendChild(span);
+//   }
+
+//   setTimeout(() => {
+//     const spans = textElement.querySelectorAll("span");
+//     spans.forEach((span) => {
+//       span.style.transform = "translateY(0)";
+//       span.style.opacity = "1";
+//     });
+//   }, 100);
+// });
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   const textElement = document.getElementById("verticalText1");
+//   const text = textElement.textContent;
+//   textElement.textContent = "";
+
+//   for (let i = 0; i < text.length; i++) {
+//     const span = document.createElement("span");
+//     span.textContent = text[i];
+//     textElement.appendChild(span);
+//     if (i < text.length - 1) {
+//       textElement.appendChild(document.createElement("br"));
+//     }
+//   }
+// });
+// document.addEventListener("DOMContentLoaded", () => {
+//   const textElement = document.getElementById("verticalText2");
+//   const text = textElement.textContent;
+//   textElement.textContent = "";
+
+//   for (let i = 0; i < text.length; i++) {
+//     const span = document.createElement("span");
+//     span.textContent = text[i];
+//     textElement.appendChild(span);
+//     if (i < text.length - 1) {
+//       textElement.appendChild(document.createElement("br"));
+//     }
+//   }
+// });
+
+// 이 때 스타일은 잘 적용되나 애니는 적용 x
+// document.addEventListener("DOMContentLoaded", () => {
+//   const textElement = document.getElementById("verticalText");
+//   const text = textElement.textContent;
+//   textElement.textContent = "";
+
+//   for (let i = 0; i < text.length; i++) {
+//     const span = document.createElement("span");
+//     span.textContent = text[i];
+//     textElement.appendChild(span);
+//   }
+// });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const textElement = document.getElementById("verticalText");
+  const text = textElement.textContent;
+  textElement.textContent = "";
+
+  for (let i = 0; i < text.length; i++) {
+    const span = document.createElement("span");
+    span.textContent = text[i];
+    span.style.display = "inline-block";
+    span.style.transform = "translateY(-100%)";
+    span.style.opacity = "0";
+    span.style.animation = `slideDown 0.5s ease forwards`;
+    span.style.animationDelay = `${i * 0.1}s`;
+    textElement.appendChild(span);
+  }
+});
+
 // ////////// ////////// ////////// ////////// ////////// //////////
 // Section 4
 // 메뉴 버튼 클릭 이벤트
@@ -184,4 +292,101 @@ clickableImages.forEach((image) => {
     event.preventDefault();
     throttle(goToNextSection, 2000); // throttle 함수를 사용하여 이벤트 처리 간격을 조정할 수 있습니다.
   });
+});
+
+// mouse cursor (click)
+const cardConAll = document.querySelectorAll(".yj-main-s4-container");
+const mouseicn = document.querySelector(".yj-main-s4-cursor");
+
+cardConAll.forEach((card) => {
+  card.addEventListener("mouseenter", function (event) {
+    mouseicn.querySelector("img").src = "imgFolder/yj-main-s4-click.png";
+    card.style.cursor = "none";
+    mouseicn.classList.remove("yj-main-s4-cursor-none");
+    mouseicn.classList.add("yj-main-s4-cursor-block");
+  });
+
+  card.addEventListener("mouseleave", function () {
+    mouseicn.classList.remove("yj-main-s4-cursor-block");
+    mouseicn.classList.add("yj-main-s4-cursor-none");
+  });
+});
+
+document.addEventListener("mousemove", (event) => {
+  const mouseX = event.clientX;
+  const mouseY = event.clientY;
+  mouseicn.style.left = mouseX + "px";
+  mouseicn.style.top = mouseY + "px";
+});
+
+// ////////// ////////// ////////// ////////// ////////// //////////
+// Section 5
+
+// mouse cursor (scroll)
+const s5CardConAll = document.querySelectorAll(".yj-main-s5-bigCon");
+const s5Mouseicn = document.querySelector(".yj-main-s5-cursor");
+
+s5CardConAll.forEach((card) => {
+  card.addEventListener("mouseenter", function (event) {
+    s5Mouseicn.querySelector("img").src = "imgFolder/yj-main-s5-scroll.png";
+    card.style.cursor = "none";
+    s5Mouseicn.classList.remove("yj-main-s5-cursor-none");
+    s5Mouseicn.classList.add("yj-main-s5-cursor-block");
+  });
+
+  card.addEventListener("mouseleave", function () {
+    s5Mouseicn.classList.remove("yj-main-s5-cursor-block");
+    s5Mouseicn.classList.add("yj-main-s5-cursor-none");
+  });
+});
+
+document.addEventListener("mousemove", (event) => {
+  const mouseX = event.clientX;
+  const mouseY = event.clientY;
+  s5Mouseicn.style.left = mouseX + "px";
+  s5Mouseicn.style.top = mouseY + "px";
+});
+
+// ////////// ////////// ////////// ////////// ////////// //////////
+// Section 6
+const s6menuBtn = document.getElementById("yj-main-s6-menuBtn");
+const s6menuBtn1 = "imgFolder/yj-main-default-menu1.png";
+const s6menuBtn2 = "imgFolder/yj-main-default-menu2.png";
+
+// 메뉴 이미지 클릭 이벤트 리스너 등록
+s6menuBtn.addEventListener("click", function () {
+  // 현재 이미지 src 확인
+  const currentSrc = this.querySelector("img").src;
+
+  // 이미지 변경
+  if (currentSrc.includes(s6menuBtn1)) {
+    this.querySelector("img").src = s6menuBtn2;
+  } else {
+    this.querySelector("img").src = s6menuBtn1;
+  }
+});
+
+// mouse cursor (scroll)
+const s6CardConAll = document.querySelectorAll("#yj-main-s6");
+const s6Mouseicn = document.querySelector(".yj-main-s6-cursor");
+
+s6CardConAll.forEach((card) => {
+  card.addEventListener("mouseenter", function (event) {
+    s6Mouseicn.querySelector("img").src = "imgFolder/yj-main-s5-scroll.png";
+    card.style.cursor = "none";
+    s6Mouseicn.classList.remove("yj-main-s6-cursor-none");
+    s6Mouseicn.classList.add("yj-main-s6-cursor-block");
+  });
+
+  card.addEventListener("mouseleave", function () {
+    s6Mouseicn.classList.remove("yj-main-s6-cursor-block");
+    s6Mouseicn.classList.add("yj-main-s6-cursor-none");
+  });
+});
+
+document.addEventListener("mousemove", (event) => {
+  const mouseX = event.clientX;
+  const mouseY = event.clientY;
+  s6Mouseicn.style.left = mouseX + "px";
+  s6Mouseicn.style.top = mouseY + "px";
 });
