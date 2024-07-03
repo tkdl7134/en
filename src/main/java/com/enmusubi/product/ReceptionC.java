88eeb01923fc868cebb54fc3211fdbb888d571df
kagi <key9910@gmail.com>
2024-07-03 15:54:46 +0900

    Fix:tings

--- src/main/java/com/enmusubi/product/ReceptionC.java
+++ src/main/java/com/enmusubi/product/ReceptionC.java
@@ -8,8 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.enmusubi.member.MemberDAO;
-
 @WebServlet("/ReceptionC")
 public class ReceptionC extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
